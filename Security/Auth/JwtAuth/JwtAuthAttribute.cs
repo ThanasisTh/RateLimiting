f6f2c42793ea62b38b5315df787fd05f3de55de1
ThanasisTh <thanasisdtl@gmail.com>
2020-12-12 16:52:31 +0100

    Moved BasicAth attribute/filter functionality

--- Security/Auth/JwtAuth/JwtAuthAttribute.cs
+++ Security/Auth/JwtAuth/JwtAuthAttribute.cs
@@ -3,12 +3,29 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using System;
 using RateLimiting.Security.Entities;
+using RateLimiting.Security.Services;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace RateLimiting.Security.Auth.JwtAuth
 {
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    public class JwtAuthAttribute : Attribute, IAuthorizationFilter
+    public class JwtAuthAttribute : Attribute, IAuthorizationFilter, IActionFilter
     {
+        public void OnActionExecuted(ActionExecutedContext context) {}
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            int len = 0;
+            var rateLimitService = context.HttpContext.RequestServices.GetRequiredService<IRateLimitService>(); 
+            try {
+                len = Int32.Parse(context.HttpContext.Request.Query["len"]);
+            }
+            catch (FormatException) {
+                ReturnBadResult(context);
+            }
+            rateLimitService.processRequest(len);
+        }
+
         public void OnAuthorization(AuthorizationFilterContext context)
         {
             var user = (User)context.HttpContext.Items["User"];
@@ -17,6 +34,11 @@ namespace RateLimiting.Security.Auth.JwtAuth
                 // not logged in
                 context.Result = new JsonResult(new { message = "Unauthorized" }) { StatusCode = StatusCodes.Status401Unauthorized };
             }
+        }
+
+        private void ReturnBadResult(ActionExecutingContext context)
+        {
+            context.Result = new BadRequestResult();
         }
     }
 }
--- Security/Services/RateLimitService.cs
+++ Security/Services/RateLimitService.cs
@@ -1,33 +1,34 @@
-
-
 namespace RateLimiting.Security.Services
 {
     public interface IRateLimitService
     {
-        int processRequest();
+        int processRequest(int bytes);
+        int reportBandwidth();
     }
 
     public class RateLimitService : IRateLimitService
     {
         private int _limit = 1024;
+        private int _bandwidth = 1024;
 
         public RateLimitService() {}
 
-        public int processRequest() {
-            return _limit;
-        }
-
-        private int useBandwidth(int bytes) {
-            if (_limit > bytes) {
-                _limit -= bytes;
-                return _limit;
+        public int processRequest(int bytes) {
+            if (_bandwidth > bytes) {
+                _bandwidth -= bytes;
+                return _bandwidth;
             };
             return -1;
         }
 
+        public int reportBandwidth()
+        {
+            return _bandwidth;
+        }
+
         private int resetBandwidth() {
-            _limit = 1024;
-            return _limit;
+            _bandwidth = _limit;
+            return _bandwidth;
         }
 
     }
