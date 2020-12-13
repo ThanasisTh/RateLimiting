ad43a0417ad33725de21135db0a842ecfa69b41d
ThanasisTh <thanasisdtl@gmail.com>
2020-12-14 00:21:15 +0100

    Added Controller request documentation

--- Controllers/RandomController.cs
+++ Controllers/RandomController.cs
@@ -26,7 +26,10 @@ namespace RateLimiting.Controllers
             DateTime resetTime = DateTime.UtcNow.AddSeconds(100);
         }
 
-
+        /// <summary>
+        /// Initial request, register a <see cref="RateLimiting.Security.Entities.User"/> to the DB. 
+        /// </summary>
+        /// <param name="user">A <see cref="RateLimiting.Security.Entities.User"/> instance</param>
         [AllowAnonymous]
         [HttpPostAttribute("register")]
         public async Task<ActionResult<User>> registerUser(User user) {
@@ -36,7 +39,13 @@ namespace RateLimiting.Controllers
             return new CreatedResult("", new UserDTO(user));
         }
 
-        // basic authentication login, requires username + password, generates JWT which lasts 5 minutes
+        /// <summary>
+        /// Basic authentication login, requires Base64 encoded "username:password", generates JWT which lasts 5 minutes
+        /// </summary>
+        /// <returns><para>An <see cref="Models.Authentication.AuthenticateResponse" /> instance, 
+        ///          containing a JWT token used to access "/random". </para>
+        ///          <para>The JWT expires after 5 minuts. </para>
+        /// </returns>
         [BasicAuth]
         [HttpPost("authenticate")]
         public IActionResult Authenticate()
@@ -44,8 +53,13 @@ namespace RateLimiting.Controllers
             return Ok();
         }
 
-        // bearer token authentication, requires generated JWT
-        // GET: /random
+        /// <summary>
+        /// Bearer token authentication, requires generated JWT
+        /// </summary>
+        /// <param name="len">The length of the requested randomness</param>
+        /// <returns>An object containing a message and (if the request was successful)
+        ///          a random Base64 encoded string of the requested length.
+        /// </returns>
         [JwtAuth]
         [HttpGet]
         public async Task<IActionResult> GetRandom(long len = 32)
